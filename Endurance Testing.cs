62bf1bd6bf273375ee8cdc84a2a4f5c0584d3894
Rahma Bintang Pratama <rahmabintangp@gmail.com>
2024-12-08 08:39:57 +0700

    Initial commit

---
+++ Endurance Testing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Endurance_Testing
+{
+    public partial class EnduranceTestingForm : Form
+    {
+        public EnduranceTestingForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
